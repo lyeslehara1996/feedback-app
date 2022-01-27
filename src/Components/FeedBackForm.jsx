84a5c79d35edebef3280beea8ef4e6f8c21066c5
lyeslehara <lyeslehara88@gmail.com>
2022-01-27 17:51:26 +0100

    add component statSelected and her event

--- src/Components/FeedBackForm.jsx
+++ src/Components/FeedBackForm.jsx
@@ -1,22 +1,37 @@
 import React,{useState} from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
+import FeedBackSelect from './FeedBackSelect';
 const FeedBackForm = () => {
 
     const [text,setText] = useState('')
+    const [btnDisabled,setBtnDisabled] = useState(true)
+    const [rating,setRating] = useState(10)
+    const [message,setMessage] = useState('')
 
     const handleTextChange = (e) =>{
-        setText(e.target.value)
+            if(text === ''){
+                setBtnDisabled(true)
+                setMessage(null)
+            }else if( text !== '' && text.trim().length <= 10 ) {
+                setMessage('Text must be at least 10 charcteres')
+                 setBtnDisabled(true)
+            }else{
+                setMessage(null)
+                setBtnDisabled(false)
+            }
+            setText(e.target.value)
     }
     return (
         <Card>
             <form > 
                 <h2>How would you rate your service with us ?</h2>
+                  <FeedBackSelect select={(rating) => setRating(rating)} />
                  <div className="input-group">
                     <input onChange= {handleTextChange} type="text " placeholder='write a review'  value={text}/>
-                    <Button type="submit" >Send</Button> 
+                    <Button type="submit" isDisabled ={btnDisabled} >Send</Button> 
                  </div>
-
+                    {message && <div className='message'> { message } </div>}
             
             
             </form>
